51e5b3ae5042f5b1627ea2fa6a846d5c32c7d56c
Maackiaw <1742885459jy@gmail.com>
2022-05-28 23:17:08 +0800

    test_ignore_3

--- Sentinel_Chassis_A/MDK-ARM/RemoteControl/psd_adc.d
+++
@@ -1,3 +0,0 @@
-remotecontrol\psd_adc.o: ..\application\PSD_adc.c
-remotecontrol\psd_adc.o: ..\application\PSD_adc.h
-remotecontrol\psd_adc.o: ..\application\struct_typedef.h
--- Sentinel_Chassis_A/MDK-ARM/RemoteControl/startup_stm32f427xx.d
+++
@@ -1,1 +0,0 @@
-remotecontrol\startup_stm32f427xx.o: startup_stm32f427xx.s
